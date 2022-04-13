743ac4961b3ffe35b28745a67afeef7fd38140b1
Steven Williams <stevenwritescode@gmail.com>
2022-04-13 15:04:12 -0600

    center home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,12 @@
 <template>
   <div>
-    <h1 class="h1">Virtual Land Manager</h1></div>
+    <h1 class="h1 text-center pa-12">Virtual Land Manager</h1>
+    <h1 class="h3 text-center pa-12">Alpha Version</h1>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "Home",
-};
+  name: 'Home'
+}
 </script>
