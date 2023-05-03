5f30483df013ebf85db8311563b3f1b68beb48f7
Unknower <dev@unknower.com>
2023-05-02 23:10:29 -0400

    add minification

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   transpileDependencies: ["vuetify", "vue-tel-input-vuetify"],
+  productionSourceMap: false,
+  configureWebpack: {
+    optimization: {
+      minimize: true,
+    },
+  },
 };
